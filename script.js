08ea54a4cb05b56f1ab90ed87ba9520aef0be886
willowisp01 <132592621+willowisp01@users.noreply.github.com>
2025-01-10 14:37:58 +0800

    Perform operations on numbers
    The calculator can now perform operations on numbers and update the
    display.

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    if (b === 0) {
-        throw new Error('Cannot divide by zero!!!');
-    }
+    // if (b === 0) {
+    //     throw new Error('Cannot divide by zero!!!');
+    // }
     return a / b;
 }
 
@@ -30,11 +30,48 @@ function updateDisplay(content) {
     display.textContent = content; 
 }
 
+// makes a calculation using que
+
+// storing the previous numbers and ops
+const numQueue = [];  //enq: queue.push(), deq: queue.shift();
+const opQueue = []; 
+
 // parses the inputs (and calls the needed functions)
 function parse(event) {
+    // parse number
     if (event.target.classList.contains("numeral")) {
         rawInput += event.target.textContent; 
         updateDisplay(rawInput);
+    } else if (event.target.classList.contains("operation") && (rawInput != "")) {
+        // always parse number typed before the operation, and push op and number to respective queues
+        numQueue.push(+rawInput);
+        rawInput = ""; 
+        opQueue.push(event.target.textContent);
+        if (numQueue.length >= 2) { // have 2 numbers to perform a calculation 
+            let a = numQueue.shift();
+            let b = numQueue.shift();
+            let op = opQueue.shift();
+            let result; 
+            // console.log(a);
+            // console.log(b);
+            // console.log(op);
+            switch (op) {
+                case '+':
+                    result = add(a, b);
+                    break;
+                case '-':
+                    result = subtract(a, b); 
+                    break;
+                case '*':
+                    result = multiply(a, b);
+                    break;
+                case '/':
+                    result = divide(a, b); //TODO: handle divide by 0\
+                    break;
+            }
+            numQueue.push(result);
+            updateDisplay(result);
+        } 
     }
 }
 
